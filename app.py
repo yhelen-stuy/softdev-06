621f8107cfc6e869698acc5528c2d859dcf7edee
Helen Ye <00yhelen@gmail.com>
2017-10-03 13:05:04 -0400

    Add comments

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+'''
+Helen Ye
+SoftDev pd7
+HW06 Echo
+2017-10-02
+'''
+
+#import necessities
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+#Create a home route for the form
 @app.route("/")
 def home():
     return render_template("home.html");
 
+#Give a response according to what was inputted
 @app.route("/greetings")
 def greetings():
     return render_template("greetings.html", username = request.args['username'],
